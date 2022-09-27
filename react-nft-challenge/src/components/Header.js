8fe5420c414d3bc6ceb8cf524df7ef202c0eaa86
Deepanshu Gupta <312.gupta@gmail.com>
2022-09-27 17:40:31 -0400

    added search bar in navbar

--- react-nft-challenge/src/components/Header.js
+++ react-nft-challenge/src/components/Header.js
@@ -1,12 +1,23 @@
 import React from "react";
 import "./header.css";
 import punkLogo from "../assets/header/cryptopunk-logo.png";
+import searchIcon from "../assets/header/search.png";
 
 const Header = () => {
   return (
     <div className="header">
       <div className="logoContainer">
         <img src={punkLogo} className="punkLogo" alt=""></img>
+      </div>
+
+      <div className="searchBar">
+        <div className="searchIconContainer">
+          <img src={searchIcon}></img>
+        </div>
+        <input
+          className="searchInput"
+          placeholder="Collection, item or user ..."
+        ></input>
       </div>
     </div>
   );
