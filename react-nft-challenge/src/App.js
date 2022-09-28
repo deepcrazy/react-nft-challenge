6a51707fd62eaf2c46bf3bcfb965b40ee9df442b
Deepanshu Gupta <312.gupta@gmail.com>
2022-09-28 17:04:57 -0400

    - added single punkListCard with props and styling - added punks folder with punk images

--- react-nft-challenge/src/App.js
+++ react-nft-challenge/src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import CollectionCard from "./components/CollectionCard";
 import Header from "./components/Header";
+import p1 from "./punks/1.jpg";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <CollectionCard
+        id={0}
+        name={"Bandana Punk"}
+        traits={[{ value: 7 }]}
+        image={p1}
+      />
     </div>
   );
 }
