fafb8cd933932481bb49cfa66c17a0b1fce65fb6
Deepanshu Gupta <312.gupta@gmail.com>
2022-09-28 19:00:46 -0400

    added crypto punk lists from opensea api and rendered component for all punks

--- react-nft-challenge/src/App.js
+++ react-nft-challenge/src/App.js
@@ -2,8 +2,25 @@ import "./App.css";
 import CollectionCard from "./components/CollectionCard";
 import Header from "./components/Header";
 import p1 from "./punks/1.jpg";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import PunkList from "./components/PunkList";
 
 function App() {
+  const [punkListData, setPunkListData] = useState([]);
+
+  // "https://testnets-api.opensea.io/assets?asset_contract_address=0xC36d211Da64a4cDD727F722196545Ec8799BeD9e&order_direction=asc"
+  useEffect(() => {
+    const getMyNfts = async () => {
+      const openseaData = await axios.get(
+        "https://testnets-api.opensea.io/assets?asset_contract_address=0x4AC148f98839F83aA3C85C79B7963c4943B34629&order_direction=asc"
+      );
+      console.log(openseaData.data.assets);
+      setPunkListData(openseaData.data.assets);
+    };
+
+    getMyNfts();
+  }, []);
   return (
     <div className="app">
       <Header />
@@ -13,6 +30,7 @@ function App() {
         traits={[{ value: 7 }]}
         image={p1}
       />
+      <PunkList punkListData={punkListData} />
     </div>
   );
 }
